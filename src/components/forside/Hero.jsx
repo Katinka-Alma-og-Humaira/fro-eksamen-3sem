ffaf65a9d8b8b715534a9474e7e86cb1709f494e
hube0001 <hube0001@stud.kea.dk>
2026-05-06 12:23:55 +0200

    Fetch and display hero image from API in Hero component

--- src/components/forside/Hero.jsx
+++ src/components/forside/Hero.jsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 
-const Hero = () => {
+const Hero = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events`);
+  const events = await response.json();
+
+  const imageUrl = `${process.env.NEXT_PUBLIC_API_URL}${events.heroAsset.url}`;
+  const hero = events[0];
+
   return (
     <div>
-      <Image src="/img/dummy.png" width={300} height={300} />
+      <Image src={imageUrl} alt={hero.alt} width={300} height={300} />
     </div>
   );
 };
