c5c4785267f67ced0b7b04a4c0789af5de5abdac
hube0001 <hube0001@stud.kea.dk>
2026-05-11 10:18:10 +0200

    Add framer-motion for animations and update Hero component layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,7 @@ import Hero from "@/components/forside/Hero";
 export default function Home() {
   return (
     <div>
-      <p className="position=relative">NIGHTCLUB</p>
-      <Hero className="position=absolute" />
+      <Hero />
     </div>
   );
 }
