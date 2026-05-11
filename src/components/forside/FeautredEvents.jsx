47650e8ed34520488a737af147366ee3f75643c7
alma05858 <alca0004@stud.kea.dk>
2026-05-11 13:18:32 +0200

    Refactor FeaturedEvents component layout and styling for improved presentation

--- src/components/forside/FeautredEvents.jsx
+++ src/components/forside/FeautredEvents.jsx
@@ -13,12 +13,25 @@ const FeautredEvents = async () => {
   const feauturedevents = await getEvents();
   console.log(feauturedevents);
   return (
-    <div className="flex flex-col justify-center items-center gap-3 py-10 m-5">
+    <div className="flex flex-col md:flex-row gap-8 items-center justify-center">
       {feauturedevents.map((event) => (
-        <div key={event.id} className="flex flex-col justify-center items-center gap-3">
-          <Image src={`${process.env.NEXT_PUBLIC_API_URL}${event.asset.url}`} alt={event.asset.alt} width={200} height={50} />
-          <h2>{event.title}</h2>
-          <p className="text-center">{event.excerpt}</p>
+        <div key={event.id} className="relative group w-[400px]">
+          <div className="relative overflow-hidden">
+            <Image className="w-[400px]" src={`${process.env.NEXT_PUBLIC_API_URL}${event.asset.url}`} alt={event.asset.alt} width={300} height={50} />
+            <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+            <div>
+              <button>BOOK NOW</button>
+            </div>
+            x
+            <div className="absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center translate-y-full group-hover:translate-y-0 transition-all duration-300 bg-black p-2">
+              <h2>{event.title}</h2>
+              <p className="text-center">{event.description}</p>
+            </div>
+          </div>
+          <div className="flex justify-between p-1 bg-[var(--color-pink)]">
+            <h2>{event.location}</h2>
+            <h2>{event.date}</h2>
+          </div>
         </div>
       ))}
     </div>
